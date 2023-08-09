0837cf9d2917aad19577963ca3b976a43ca390f4
Joe Sandmeyer <github.com@unconscious.com>
2023-08-09 13:12:20 -0400

    Create Diagnostics.tsx

---
+++ src/components/Diagnostics.tsx
@@ -0,0 +1,44 @@
+//import { Outlet, useOutletContext } from 'react-router-dom';
+//import './diagTypes.tsx';
+import '../Common.css';
+
+
+export interface DiagConfig {
+  showLayoutNames:		boolean;
+  logOnRender:				boolean;
+}
+
+
+export function DiagStamp(props) {
+	//const { diagConfig, stampText } = useOutletContext();
+	let stampElement: JSX.Element = (!! props.diagConfig.showLayoutNames)
+		? <div className='diagnostic_text' >{props.stampText}</div> : <div/>;
+
+  // console.log('In DiagStamp', props);
+
+  return (
+		<div>
+			{ (!! props.diagConfig.logOnRender) ? console.log(`Render ${props.stampText}`) : <div/> }
+			{ stampElement }
+		</div>
+  );
+}
+
+
+/***
+// Usage:
+
+import { DiagConfig, DiagStamp } from '../components/Diagnostics';
+import { Outlet, useOutletContext } from 'react-router-dom';
+
+
+	const { diagConfig } = useOutletContext();
+
+	return (
+		<div>
+			<DiagStamp stampText='ThisComponentName' diagConfig={diagConfig} />
+			<Outlet context={{diagConfig}}/>			
+		</div>
+	);
+
+ ***/
