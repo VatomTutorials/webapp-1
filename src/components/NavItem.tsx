d303986ed0e093c733107de61cbbed9766f30f16
Joe Sandmeyer <github.com@unconscious.com>
2023-08-18 12:39:52 -0400

    Add logout button

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,31 +1,30 @@
-import { NavItemProps } from "./NavTypes"
-import Button from 'react-bootstrap/Button';
-import '../Common.css';
-
+import { NavItemProps } from "./NavTypes";
+import Button from "react-bootstrap/Button";
+import "../Common.css";
 
 interface PropsContainer {
-  navItemProps:		NavItemProps;
+  navItemProps: NavItemProps;
 }
 
-
 export function NavItem({ navItemProps }: PropsContainer) {
-	const buttonClass: string = (navItemProps.enabled)
-		? "navbar_button_enabled" : "navbar_button_disabled";
-	
+  const buttonClass: string = navItemProps.enabled
+    ? "navbar_button_enabled"
+    : "navbar_button_disabled";
+
   return (
     <>
       <Button
-      	key={navItemProps.config.route}
-      	className={buttonClass}
-      	disabled={(! navItemProps.enabled)}
-				onClick={() => {
-					if (navItemProps.enabled){
-						navItemProps.onNavChange(navItemProps.config.route);
-					}
-				}}
-			>
-      	{navItemProps.config.label}
+        key={navItemProps.config.route}
+        className={buttonClass}
+        disabled={!navItemProps.enabled}
+        onClick={() => {
+          if (navItemProps.enabled) {
+            navItemProps.onNavChange(navItemProps.config.route);
+          }
+        }}
+      >
+        {navItemProps.config.label}
       </Button>
     </>
-  )
+  );
 }
--- src/route-priv/PrivLogout.tsx
+++ src/route-priv/PrivLogout.tsx
@@ -1,5 +1,7 @@
 import { useRootContext_FromOutletContext } from "../route-core/RootLayout";
 import { DiagStamp } from "../components/Diagnostics";
+import Button from "react-bootstrap/Button";
+import "../Common.css";
 
 const PrivLogout = () => {
   const { rootContext } = useRootContext_FromOutletContext();
@@ -8,6 +10,15 @@ const PrivLogout = () => {
     <div>
       <DiagStamp stampText="PrivLogout" diagConfig={rootContext.diagConfig} />
       <h1>Logout</h1>
+      <Button
+        key="btn01"
+        className="plain_button_enabled"
+        onClick={() => {
+          console.log("Click for logout");
+        }}
+      >
+        log out now
+      </Button>
     </div>
   );
 };
